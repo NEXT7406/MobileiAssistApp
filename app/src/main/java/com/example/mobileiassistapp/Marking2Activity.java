3174f8c7e2d4eeeda889f1f3ef8828453c264caf
nmsNish <sanduninisha19@gmail.com>
2020-09-23 22:35:06 +0530

    marking day2

--- app/src/main/java/com/example/mobileiassistapp/Marking2Activity.java
+++ app/src/main/java/com/example/mobileiassistapp/Marking2Activity.java
@@ -6,18 +6,28 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class Marking2Activity extends AppCompatActivity {
 
     Button btn_save;
+    TextView ca;
+    TextView total;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.marking2activity);
+        Bundle bundle = getIntent().getExtras();
 
-        btn_save = findViewById(R.id.btn_save);
+        btn_save = (Button) findViewById(R.id.btn_save);
+        ca = (TextView) findViewById(R.id.ca_text_id);
+        total = (TextView) findViewById(R.id.total_text_id);
 
+        float ca_marks = bundle.getFloat("ca");
+        float final_marks = bundle.getFloat("final");
+        ca.setText(Float.toString(ca_marks));
+        total.setText(Float.toString((final_marks+ca_marks)));
         btn_save.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
--- app/src/main/java/com/example/mobileiassistapp/MarkingActivity.java
+++ app/src/main/java/com/example/mobileiassistapp/MarkingActivity.java
@@ -6,10 +6,15 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class MarkingActivity extends AppCompatActivity {
 
     Button btn_cal;
+    EditText assg1;
+    EditText assg2;
+    EditText mid;
+    EditText finalExam;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,12 +22,20 @@ public class MarkingActivity extends AppCompatActivity {
         setContentView(R.layout.activity_marking);
 
         btn_cal = findViewById(R.id.btn_cal);
+        assg1 = (EditText)findViewById(R.id.ass1);
+        assg2 = (EditText)findViewById(R.id.ass2);
+        mid = (EditText)findViewById(R.id.mid_marks);
+        finalExam = (EditText)findViewById(R.id.final_marks);
 
         btn_cal.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
+                float CAMarks = (Float.parseFloat(assg1.getText().toString())/100)*15 + (Float.parseFloat(assg2.getText().toString())/100)*15 + (Float.parseFloat(mid.getText().toString())/100)*20;
+                float FinalMarks = (Float.parseFloat(finalExam.getText().toString())/100)*50;
                 Intent intent = new Intent(MarkingActivity.this, Marking2Activity.class);
+                intent.putExtra("ca",CAMarks);
+                intent.putExtra("final",FinalMarks);
                 startActivity(intent);
 
             }
