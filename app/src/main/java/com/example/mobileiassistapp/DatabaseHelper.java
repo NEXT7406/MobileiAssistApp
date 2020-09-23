f5b478d8a27af51f6012e4f9d95ac87817aaeef8
nmsNish <sanduninisha19@gmail.com>
2020-09-24 00:09:59 +0530

    marking day2 - insert data completed

---
+++ app/src/main/java/com/example/mobileiassistapp/DatabaseHelper.java
@@ -0,0 +1,47 @@
+package com.example.mobileiassistapp;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+public class DatabaseHelper extends SQLiteOpenHelper {
+    public static final String DATABASE_NAME = "Mad.db";
+    public static final String TABLE_NAME = "subject_table";
+    public static final String COL_1 = "ID";
+    public static final String COL_2 = "SUBJECT_ID";
+    public static final String COL_3 = "SUBJECT_NAME";
+    public static final String COL_4 = "CA_MARKS";
+    public static final String COL_5 = "TOTAL_MARKS";
+
+    public DatabaseHelper(Context context) {
+        super(context, DATABASE_NAME, null, 1);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+    db.execSQL("create table "+TABLE_NAME+"(ID INTEGER PRIMARY KEY AUTOINCREMENT,SUBJECT_ID TEXT, SUBJECT_NAME TEXT, CA_MARKS FLOAT, TOTAL_MARKS FLOAT)");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+    db.execSQL("DROP TABLE IF EXISTS "+TABLE_NAME);
+    }
+
+    public boolean insertData(String subject_id,String subject_name,Float ca, float total){
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(COL_2,subject_id);
+        contentValues.put(COL_3,subject_name);
+        contentValues.put(COL_4,ca);
+        contentValues.put(COL_5,total);
+        long result = db.insert(TABLE_NAME,null,contentValues);
+        if(result == -1){
+            return false;
+        }else{
+            return true;
+        }
+    }
+}
--- app/src/main/java/com/example/mobileiassistapp/Marking2Activity.java
+++ app/src/main/java/com/example/mobileiassistapp/Marking2Activity.java
@@ -6,36 +6,62 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class Marking2Activity extends AppCompatActivity {
 
     Button btn_save;
+    Button btn_view_all;
     TextView ca;
     TextView total;
-
+    EditText subject_id;
+    EditText subject_name;
+    DatabaseHelper myDb;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.marking2activity);
         Bundle bundle = getIntent().getExtras();
 
+        myDb = new DatabaseHelper(this);
+
         btn_save = (Button) findViewById(R.id.btn_save);
+        btn_view_all = (Button) findViewById(R.id.view_all_btn);
         ca = (TextView) findViewById(R.id.ca_text_id);
         total = (TextView) findViewById(R.id.total_text_id);
+        subject_id = (EditText) findViewById(R.id.subject_id);
+        subject_name = (EditText) findViewById(R.id.subject_name);
 
         float ca_marks = bundle.getFloat("ca");
         float final_marks = bundle.getFloat("final");
-        ca.setText(Float.toString(ca_marks));
-        total.setText(Float.toString((final_marks+ca_marks)));
-        btn_save.setOnClickListener(new View.OnClickListener() {
+        ca.setText(String.format("%.2f",ca_marks));
+        total.setText(String.format("%.2f",final_marks));
+        /*btn_view_all.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
-                startActivity(new Intent(Marking2Activity.this, Marking3Activity.class));
+        startActivity(new Intent(Marking2Activity.this, Marking4Activity.class));
 
             }
-        });
+        });*/
+        AddData();
+    }
 
+    public void AddData(){
+        btn_save.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                    boolean isInserted =  myDb.insertData(subject_id.getText().toString(),subject_name.getText().toString(),Float.parseFloat(ca.getText().toString()),Float.parseFloat(total.getText().toString()));
+                    if(isInserted == true){
+                        Toast.makeText(Marking2Activity.this,"Data Inserted",Toast.LENGTH_LONG).show();
+                    }else{
+                        Toast.makeText(Marking2Activity.this,"Data Inserted",Toast.LENGTH_LONG).show();
+                    }
+                    }
+                }
+        );
     }
 }
