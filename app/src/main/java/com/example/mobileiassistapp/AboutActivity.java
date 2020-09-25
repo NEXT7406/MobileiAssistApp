6f7a81cd31ba49b5e07e619c355045d47c901521
NEXT7406 <dilushaperera24@gmail.com>
2020-09-25 23:45:54 +0530

    5th Commit

--- app/src/main/java/com/example/mobileiassistapp/AboutActivity.java
+++ app/src/main/java/com/example/mobileiassistapp/AboutActivity.java
@@ -2,13 +2,28 @@ package com.example.mobileiassistapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class AboutActivity extends AppCompatActivity {
+    Button btnFb;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+
+        btnFb = (Button)findViewById(R.id.fbButton);
+        btnFb.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.facebook.com/mobileiassist"));
+                startActivity(browserIntent);
+            }
+        });
     }
 }
