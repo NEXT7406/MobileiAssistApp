d153cde92f680e87bc391184f6812ba30fe879ff
gayara <gayarakumari366@gmail.com>
2020-09-26 21:38:36 +0530

    commit

---
+++ app/src/main/java/com/example/mobileiassistapp/SaveGpa.java
@@ -0,0 +1,25 @@
+package com.example.mobileiassistapp;
+
+public class SaveGpa {
+    private String title;
+    private double gpa;
+
+    public SaveGpa() {
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public double getGpa() {
+        return gpa;
+    }
+
+    public void setGpa(double gpa) {
+        this.gpa = gpa;
+    }
+}
--- app/src/main/java/com/example/mobileiassistapp/saveGPAActivity.java
+++ app/src/main/java/com/example/mobileiassistapp/saveGPAActivity.java
@@ -1,52 +1,72 @@
 package com.example.mobileiassistapp;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.ContentValues;
-import android.content.Context;
 import android.content.Intent;
-import android.database.sqlite.SQLiteDatabase;
-import android.database.sqlite.SQLiteOpenHelper;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
-import com.example.mobileiassistapp.Database.DBHelper;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class saveGPAActivity extends AppCompatActivity {
+    EditText txt1,txt2;
+    Button save,update,delete,show,home;
+    DatabaseReference dbRef;
+    SaveGpa gpa;
 
 
-    EditText title;
-    EditText gpa;
-    DBHelper db;
-    Button save;
 
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_save_g_p_a);
 
+        txt1=findViewById(R.id.editTitle);
+        txt2=findViewById(R.id.editGPA);
+
+        save=findViewById(R.id.button5);
+        update=findViewById(R.id.button6);
+        delete=findViewById(R.id.button7);
+        show=findViewById(R.id.button9);
+        home=findViewById(R.id.button10);
+
+        gpa=new SaveGpa();
+        dbRef=FirebaseDatabase.getInstance().getReference().child("savegpa");
 
 
-        title=(EditText)findViewById(R.id.editTitle);
-        gpa=(EditText)findViewById(R.id.editGPA);
-        save=(Button)findViewById(R.id.button5);
-        db=new DBHelper(this);
 
         save.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String value1=title.getText().toString();
-                Double value2= Double.valueOf(gpa.getText().toString());
+                try {
+                    if (TextUtils.isEmpty(txt1.getText().toString()))
+                        Toast.makeText(getApplicationContext(), "Empty title", Toast.LENGTH_SHORT).show();
+                    else if (TextUtils.isEmpty(txt2.getText().toString()))
+                        Toast.makeText(getApplicationContext(), "Empty gpa", Toast.LENGTH_SHORT).show();
+                    else {
 
-                Boolean checkinsertdata=db.insertData(value1,value2);
-                if(checkinsertdata==true){
-                    Toast.makeText(saveGPAActivity.this,"INSERTED SUCCESSFULLY!",Toast.LENGTH_SHORT).show();
-                }else{
-                    Toast.makeText(saveGPAActivity.this,"DATA NOT INSERTED!",Toast.LENGTH_SHORT).show();
+                        gpa.setTitle(txt1.getText().toString().trim());
+                        gpa.setGpa(Double.parseDouble(txt2.getText().toString().trim()));
+                        dbRef.child("save1").setValue(gpa);
+
+                        Toast.makeText(saveGPAActivity.this, "Successfully Inserted!", Toast.LENGTH_SHORT).show();
+                        clearControl();
+                    }
+                } catch (NumberFormatException num) {
+                    Toast.makeText(getApplicationContext(), "Invalid gpa", Toast.LENGTH_SHORT).show();
+
                 }
             }
         });
@@ -54,16 +74,82 @@ public class saveGPAActivity extends AppCompatActivity {
 
 
 
+        show.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dbRef=FirebaseDatabase.getInstance().getReference().child("savegpa/save1");
+                dbRef.addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                        if(dataSnapshot.hasChildren()) {
+                            txt1.setText(dataSnapshot.child("title").getValue().toString());
+                            txt2.setText(dataSnapshot.child("gpa").getValue().toString());
+
+                        }else
+                            Toast.makeText(getApplicationContext(),"Cannot find save1",Toast.LENGTH_SHORT).show();
+
+
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                    }
+                });
+
+
+
+            }
+        });
+
+        update.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                dbRef=FirebaseDatabase.getInstance().getReference();
+                dbRef.child("savegpa").child("save1").child("gpa").setValue(txt2.getText().toString().trim());
+                Toast.makeText(getApplicationContext(),"Successfully Updated!",Toast.LENGTH_SHORT).show();
+                clearControl();
+
+
+            }
+        });
+
+
+    delete.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            dbRef=FirebaseDatabase.getInstance().getReference().child("savegpa").child("save1");
+            dbRef.removeValue();
+            Toast.makeText(getApplicationContext(),"Successfully Deleted!",Toast.LENGTH_SHORT).show();
+            clearControl();
+        }
+    });
+
+
+
+        home.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openHomeActivity();
+            }
+        });
+
+
 
 
     }
 
-    public void openhistoryActivity() {
-        Intent intent = new Intent(this, historyActivity.class);
-        startActivity(intent);
+    private void clearControl(){
 
+        txt1.setText("");
+        txt2.setText("");
     }
 
-
+    //navigate to the home page when clicking on home button
+    public void openHomeActivity() {
+        Intent intent1 = new Intent(this, HomeActivity.class);
+        startActivity(intent1);
+    }
 
 }
