c7d7afb5871b20f4c602208131a0e46adbc4d43b
NEXT7406 <68704895+NEXT7406@users.noreply.github.com>
2020-10-01 01:07:56 +0530

    Revert "Sanduni's Branch Merge with Master"

--- app/src/main/java/com/example/mobileiassistapp/MarkingActivity.java
+++ app/src/main/java/com/example/mobileiassistapp/MarkingActivity.java
@@ -2,110 +2,13 @@ package com.example.mobileiassistapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.text.TextUtils;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
-import android.widget.Toast;
 
 public class MarkingActivity extends AppCompatActivity {
-
-    Button btn_cal;
-    EditText assg1;
-    EditText assg2;
-    EditText mid;
-    EditText finalExam;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_marking);
-
-        btn_cal = findViewById(R.id.btn_cal);
-        assg1 = (EditText)findViewById(R.id.ass1);
-        assg2 = (EditText)findViewById(R.id.ass2);
-        mid = (EditText)findViewById(R.id.mid_marks);
-        finalExam = (EditText)findViewById(R.id.final_marks);
-
-        btn_cal.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                float CAMarks = calculateCAMarks(Float.parseFloat(assg1.getText().toString()),
-                        Float.parseFloat(assg2.getText().toString()),Float.parseFloat(mid.getText().toString()));
-                float FinalMarks = calculateFinalMarks(Float.parseFloat(finalExam.getText().toString()));
-                Intent intent = new Intent(MarkingActivity.this, Marking2Activity.class);
-                intent.putExtra("ca",CAMarks);
-                intent.putExtra("final",FinalMarks);
-                startActivity(intent);
-
-            }
-        });
-
-        assg1.setOnFocusChangeListener(new View.OnFocusChangeListener() {
-            @Override
-            public void onFocusChange(View v, boolean hasFocus) {
-                if(!TextUtils.isEmpty(assg1.getText().toString())){
-                    if(!hasFocus && Float.parseFloat(assg1.getText().toString())>100){
-                        Toast.makeText(getApplicationContext(), "Marks should be less than 100", Toast.LENGTH_LONG).show();
-                    }
-                }else{
-                    Toast.makeText(getApplicationContext(), "Marks can not be empty", Toast.LENGTH_LONG).show();
-                }
-            }
-        });
-
-        assg2.setOnFocusChangeListener(new View.OnFocusChangeListener() {
-            @Override
-            public void onFocusChange(View v, boolean hasFocus) {
-                if(!TextUtils.isEmpty(assg2.getText().toString())){
-                    if(!hasFocus && Float.parseFloat(assg2.getText().toString())>100){
-                        Toast.makeText(getApplicationContext(), "Marks should be less than 100", Toast.LENGTH_LONG).show();
-                    }
-                }else{
-                    Toast.makeText(getApplicationContext(), "Marks can not be empty", Toast.LENGTH_LONG).show();
-                }
-
-            }
-        });
-
-        mid.setOnFocusChangeListener(new View.OnFocusChangeListener() {
-            @Override
-            public void onFocusChange(View v, boolean hasFocus) {
-                if(!TextUtils.isEmpty(mid.getText().toString())){
-                    if(!hasFocus && Float.parseFloat(mid.getText().toString())>100){
-                        Toast.makeText(getApplicationContext(), "Marks should be less than 100", Toast.LENGTH_LONG).show();
-                    }
-                }else{
-                    Toast.makeText(getApplicationContext(), "Marks can not be empty", Toast.LENGTH_LONG).show();
-                }
-            }
-        });
-
-        finalExam.setOnFocusChangeListener(new View.OnFocusChangeListener() {
-            @Override
-            public void onFocusChange(View v, boolean hasFocus) {
-                if(!TextUtils.isEmpty(finalExam.getText().toString())){
-                    if(!hasFocus && Float.parseFloat(finalExam.getText().toString())>100){
-                        Toast.makeText(getApplicationContext(), "Marks should be less than 100", Toast.LENGTH_LONG).show();
-                    }
-                }else{
-                    Toast.makeText(getApplicationContext(), "Marks can not be empty", Toast.LENGTH_LONG).show();
-                }
-            }
-        });
     }
-
-    public float calculateCAMarks(float assg1, float assg2,float mid){
-         float ca =assg1*15/100 + assg2*15/100 + mid*20/100;
-         return ca;
-    }
-
-    public float calculateFinalMarks(float finalExamMarks){
-        float finalMarks = finalExamMarks*50/100;
-        return finalMarks;
-    }
-
 }
--- app/src/test/java/com/example/mobileiassistapp/ExampleUnitTest.java
+++ app/src/test/java/com/example/mobileiassistapp/ExampleUnitTest.java
@@ -14,46 +14,4 @@ public class ExampleUnitTest {
     public void addition_isCorrect() {
         assertEquals(4, 2 + 2);
     }
-
-    @Test
-    public void calculateCAMarks() {
-        float assg1 = 70;
-        float assg2 = 80;
-        float mid = 90;
-        float expected = Float.parseFloat("40.50");
-        float delta = Float.parseFloat("0.1");
-
-        MarkingActivity markingActivity = new MarkingActivity();
-
-        float output =  markingActivity.calculateCAMarks(assg1,assg2,mid);
-
-        assertEquals(expected,output,delta);
-    }
-
-    @Test
-    public void calculateTotal() {
-        float ca = 50;
-        float final_marks= 44;
-        float expect = 94;
-        float delta = Float.parseFloat("0.1");
-
-        Marking2Activity marking2Activity = new Marking2Activity();
-
-        float output = marking2Activity.calculateTotal(final_marks,ca);
-
-        assertEquals(expect,output,delta);
-    }
-
-    @Test
-    public void calculateFinalMarks() {
-        float finalMarksOutOf100 = 70;
-        float expect = 35;
-        float delta = Float.parseFloat("0.1");
-
-        MarkingActivity markingActivity = new MarkingActivity();
-        float output = markingActivity.calculateFinalMarks(finalMarksOutOf100);
-
-        assertEquals(expect,output,delta);
-    }
-
 }
